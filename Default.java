f145d5a86504761ad178d85f603bb87051389ad7
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2021-09-20 12:45:01 +0530

    Add files via upload

---
+++ Default.java
@@ -0,0 +1,24 @@
+public class Default
+{
+		static byte b;
+		static short s;
+		static int i;
+		static long l;
+		static float f;
+		static double d;
+		static char ch;
+		static String str;
+		static boolean bool;
+public static void main(String args[])
+{
+	System.out.println("The default values of byte is "+Default.b);
+	System.out.println("The default values of short is "+Default.s);
+	System.out.println("The default values of int is "+Default.i);
+	System.out.println("The default values of long is "+Default.l);
+	System.out.println("The default values of float is "+Default.f);
+	System.out.println("The default values of double is "+Default.d);
+	System.out.println("The default values of char is "+Default.ch);
+	System.out.println("The default values of string is "+Default.str);
+	System.out.println("The default values of boolean is "+Default.bool);	
+}
+}
