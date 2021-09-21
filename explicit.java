b60036a1901a7c23328617623439c4c2270b5e3b
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2021-09-22 00:46:17 +0530

    Add files via upload

---
+++ explicit.java
@@ -0,0 +1,18 @@
+public class explicit
+{
+public static void main(String args[])
+{
+byte b,c,e;
+int i =258,h;
+double d= 323.142;
+System.out.println("conversion int to byte");
+c=(byte)i;
+System.out.println(i+ " " +c);
+System.out.println("conversion double to int");
+h=(int)d;
+System.out.println(d+ " " +h);
+System.out.println("conversion double to byte");
+e=(byte)d;
+System.out.println(d+ " " +e);
+}
+}
