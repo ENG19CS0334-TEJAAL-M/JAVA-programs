951c74eaff430dfaffe6922e2b791f031a99b01c
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2021-09-28 17:36:17 +0530

    type conversion

---
+++ Explicitconv.java
@@ -0,0 +1,15 @@
+class Explicitconv
+{
+public static void main(String args[])
+{
+double d=35.56712311;
+int i=(int)d;
+float f=(float)d;
+long l=(long)f;
+char c =(char)i;
+short s =(short)i;
+byte b=(byte)s;
+
+System.out.println("int "+i+" float "+f+" long "+l+" char "+c+" short "+s+" byte "+b );
+}
+}
