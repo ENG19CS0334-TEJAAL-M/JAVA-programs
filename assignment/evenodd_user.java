b6b7e7f5380e66d9de4232aeebf3f19e311f6f34
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2021-11-05 11:30:28 +0530

    Create evenodd_user.java

---
+++ assignment/evenodd_user.java
@@ -0,0 +1,44 @@
+/**3.Write a program to print the sum and average of the even and odd numbers separately.**/
+import java.util.Scanner;
+
+
+public class Main
+{
+
+  public static void main (String[]args)
+  {
+    int ecount = 0, ocount = 0, esum = 0, osum = 0;
+    float eavg = 0f, oavg = 0f;
+
+    Scanner in = new Scanner (System.in);
+
+      System.out.println ("How many numbers");
+    int n = in.nextInt ();
+
+    for (int i = 1; i <= n; i++)
+      {
+          int j=in.nextInt();
+
+
+	if (j % 2 == 0)
+	  {
+	    
+	    System.out.println ("The number is even \n");
+	    esum = esum + j;
+        ecount++;
+	  }
+	else
+	  {
+	    
+	    System.out.println ("The number is odd \n");
+	    osum = osum + j;
+        ocount++;
+	  }
+      }
+
+    eavg=esum/ecount;
+    oavg=osum/ocount;
+    System.out.println ("THe sum and average of even numbers= " + esum  + " "+eavg);
+    System.out.println ("THe sum and average of odd numbers= " + osum +" "+ oavg);
+  }
+}
