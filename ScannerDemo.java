3f9376c71cb10878c1cb6764928c7b11954121a5
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2022-02-08 19:11:19 +0530

    Add files via upload

---
+++ ScannerDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ScannerDemo
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str=sc.nextLine();
+     		System.out.println("Enter the number");
+		int a=sc.nextInt();
+		System.out.println("u have entered "+str+a);
+	}
+}
+		
